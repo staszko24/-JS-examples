93702e5bd38799e87a6535705b7594d7e583f964
staszko24 <161465380+staszko24@users.noreply.github.com>
2024-06-20 13:41:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const colors = ['red','green','black','purple','#A31FF0','#FFB450'];//Colors used by function. 
+
+
+function randomValue(arr) {
+    const random = Math.floor(Math.random() * arr.length);
+    return random;
+}
+
+function setColor(fn) {
+    const elm = document.getElementById('content');
+    elm.style.backgroundColor = colors[fn];
+}
+
+console.log(randomValue(colors));//Check the index of returned value. Only for developing purpose. Delete in final version!
+
+(() => setColor(randomValue(colors)))();
+setColor(randomValue(colors))
+
+
+console.log(typeof colors);
+
